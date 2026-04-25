b076ee26c22f97904f4849e6a289fa956d5e24af
waqas-khan-codecanvas <waqaskhan11411704@gmail.com>
2026-04-25 23:19:56 +0500

    clean up

--- app/configs/app_config.py
+++
@@ -1,6 +0,0 @@
-import customtkinter as ctk
-from ..utils.constants import (DEFAULT_COLOR , DEFAULT_THEME)
-
-ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("blue")
-
--- app/pages/Home.py
+++
@@ -1,8 +0,0 @@
-import customtkinter as ctk
-
-class HomePage(ctk.CTkFrame):
-    def __init__(self , parent):
-        super().__init__(parent)
-        
-        self.label = ctk.CTkLabel(self  , text="Home page" , font=ctk.CTkFont(size=24 , weight="bold"))
-        self.label.pack(pady=40)
